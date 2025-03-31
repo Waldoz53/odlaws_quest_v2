222996ce661c8a3f0d196219d35b226f08b498a8
Waleed Rawasia <waleed@Waleeds-MacBook-Air.local>
2025-03-31 12:25:28 -0600

    added enemy and changed some code

--- main.lua
+++ main.lua
@@ -10,25 +10,47 @@ local player = {
   scale = 3,
   attackRange = 50,
   attackDamage = 10,
-  attackRate = 1.00
+  isAttacking = false,
+  attackCooldown = 1.00,
+  timeSinceLastAttack = 0
 }
+
+-- Enemy class
+local enemy = {
+  maxHP = 10,
+  currentHP = 10,
+  x = 600,
+  y = 200,
+  image = nil,
+  width = 40,
+  height = 40,
+  isAlive = true
+}
+
+-- A message "log"
+local message = ''
 
 -- Loads settings, sprites, etc
 function love.load()
   love.window.setTitle("Odlaw's Quest 2")
   love.graphics.setBackgroundColor( 0, .4, .6, .2 )
 
-  player.imageDown = love.graphics.newImage("odlaw_sprite/odlaw_down.png")
-  player.imageUp = love.graphics.newImage("odlaw_sprite/odlaw_up.png")
-  player.imageLeft = love.graphics.newImage("odlaw_sprite/odlaw_left.png")
-  player.imageRight = love.graphics.newImage("odlaw_sprite/odlaw_right.png")
-  player.imageIdle = love.graphics.newImage("odlaw_sprite/odlaw_idle.png")
+  player.imageDown = love.graphics.newImage("assets/odlaw_sprite/odlaw_down.png")
+  player.imageUp = love.graphics.newImage("assets/odlaw_sprite/odlaw_up.png")
+  player.imageLeft = love.graphics.newImage("assets/odlaw_sprite/odlaw_left.png")
+  player.imageRight = love.graphics.newImage("assets/odlaw_sprite/odlaw_right.png")
+  player.imageIdle = love.graphics.newImage("assets/odlaw_sprite/odlaw_idle.png")
   player.image = player.imageIdle
+
+  enemy.image = love.graphics.newImage("assets/greenskin.png")
 end
 
 -- Updates anything as needed
 function love.update(dt)
   local moveX, moveY = 0, 0
+
+  -- Update player attack timer
+  player.timeSinceLastAttack = player.timeSinceLastAttack + dt
 
   if love.keyboard.isDown('w') then
     moveY = -1
@@ -58,6 +80,16 @@ function love.update(dt)
   player.x = player.x + moveX * player.speed * dt
   player.y = player.y + moveY * player.speed * dt
 
+  -- handles attacks
+  if player.timeSinceLastAttack >= player.attackCooldown then
+    if love.keyboard.isDown('up') or love.keyboard.isDown('down') or love.keyboard.isDown('left') or love.keyboard.isDown('right') then
+      player.isAttacking = true
+      player.timeSinceLastAttack = 0
+    end
+  else 
+    player.isAttacking = false
+  end
+
   function love.keypressed(k)
     if k == 'escape' then
        love.event.quit()
@@ -70,17 +102,24 @@ function love.draw()
   -- Draws the player, position x, position y, 0 rotation, player scaleX, player scaleY)
   love.graphics.draw(player.image, player.x, player.y, 0, player.scale, player.scale)
 
-  -- Melee attack hitbox drawing for testing sizes
-  if love.keyboard.isDown('up') then
-    love.graphics.rectangle("fill", player.x + 32, player.y - 23, 36, player.attackRange)
-  elseif love.keyboard.isDown('down') then
-    love.graphics.rectangle("fill", player.x + 32, player.y + 68, 36, player.attackRange)
-  elseif love.keyboard.isDown('left') then
-    love.graphics.rectangle("fill", player.x - 20, player.y + 28, player.attackRange, 36)
-  elseif love.keyboard.isDown('right') then
-    love.graphics.rectangle("fill", player.x + 70, player.y + 28, player.attackRange, 36)
+  if player.isAttacking then
+    -- Melee attack hitbox drawing for testing sizes
+    if love.keyboard.isDown('up') then
+      love.graphics.rectangle("fill", player.x + 32, player.y - 23, 36, player.attackRange)
+    elseif love.keyboard.isDown('down') then
+      love.graphics.rectangle("fill", player.x + 32, player.y + 68, 36, player.attackRange)
+    elseif love.keyboard.isDown('left') then
+      love.graphics.rectangle("fill", player.x - 20, player.y + 28, player.attackRange, 36)
+    elseif love.keyboard.isDown('right') then
+      love.graphics.rectangle("fill", player.x + 70, player.y + 28, player.attackRange, 36)
+    end
   end
+
+  -- draw enemy
+  love.graphics.draw(enemy.image, enemy.x, enemy.y, 0, 2, 2)
 
   -- Draws the UI element for player HP
   love.graphics.print("HP: " .. player.currentHp .. " / " .. player.maxHp, 5, 5)
+  -- Draws the UI element for player score
+  -- love.graphics.print(message, 700, 5)
 end
